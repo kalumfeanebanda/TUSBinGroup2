55a08eb6f516ca6a2871bf9a78010db99eba8277
Favour Ojoye <149163862+favourojoye@users.noreply.github.com>
2025-09-28 17:15:43 +0100

    added name

--- tusg2frontend/src/pages/home.vue
+++ tusg2frontend/src/pages/home.vue
@@ -2,6 +2,7 @@
   <section class="container">
     <h2 class="title">Welcome to Our Project</h2>
     <p>Kalum</p>
+    <p>favour</p>
     <p class="lead">
       A quick landing page to introduce what we’re building.
     </p>
