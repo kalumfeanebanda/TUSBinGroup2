9a8d92ef10035151e1ef4a0cbbadf7fa81fbb505
kalumfeanebanda <K00300841@student.tus.ie>
2025-09-28 17:18:14 +0100

    tester

--- tusg2frontend/src/pages/home.vue
+++ tusg2frontend/src/pages/home.vue
@@ -2,6 +2,10 @@
   <section class="container">
     <h2 class="title">Welcome to Our Project</h2>
     <p>Kalum</p>
+
+
+
+    <p>tester</p>
     <p class="lead">
       A quick landing page to introduce what we’re building.
     </p>
