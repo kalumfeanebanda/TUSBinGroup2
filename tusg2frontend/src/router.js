57c328c7ceba43d6a10f4219dc58591df92039aa
Dauud10 <k00263519@student.tus.ie>
2025-10-20 19:24:25 +0100

    Router links updated for adminlogin, admindashboard pages.

--- tusg2frontend/src/router.js
+++ tusg2frontend/src/router.js
@@ -1,11 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+// User pages
 import Home from './pages/Home.vue'
-import Login from "./pages/UserLogin.vue";
+import Login from "./pages/UserLogin.vue"
 import Register from "./pages/UserRegister.vue"
+
+// Admin pages
+import AdminLogin from "./pages/AdminLogin.vue"
+import AdminDashboard from "./pages/AdminDashboard.vue"
+
 const routes = [
     { path: '/', name: 'home', component: Home },
-    {path: '/login', name: 'login',component: Login},
-    {path: '/register',name:'register',component: Register}
+    { path: '/login', name: 'login', component: Login },
+    { path: '/register', name: 'register', component: Register },
+
+
+    { path: '/admin login', name: 'admin login', component: AdminLogin },
+    { path: '/admin dashboard', name: 'admin dashboard', component: AdminDashboard },
 ]
 
 export const router = createRouter({
