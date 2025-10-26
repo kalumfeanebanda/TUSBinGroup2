efc81aa41edf5ad2eaf5af1deaca141a926d0ca1
kalumfeanebanda <K00300841@student.tus.ie>
2025-10-26 12:41:43 +0000

    unknown

---
+++ BinBackendG2/app/Controllers/Api/Bins.php
@@ -0,0 +1,21 @@
+<?php namespace App\Controllers\Api;
+
+namespace App\Controllers\Api;
+
+class Bins extends ResourceController
+{
+
+    protected $format = 'json';
+
+    public function index(){
+        $db   = \Config\Database::connect();
+        $q    = $db->query("CALL adm_Read_bins();");
+        $rows = $q->getResultArray();
+
+        // flush extra result sets after CALL
+        while ($db->connID->more_results() && $db->connID->next_result()) {}
+
+        return $this->respond(['data' => $rows]);
+    }
+
+}
